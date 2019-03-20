a48f9c253c4bfca692434353ed6b363586326813
stevensaito <saito.tan14@gmail.com>
2019-03-19 20:08:38 -0700

    pdfcrack script
    pdfcrack script

--- README.md
+++ README.md
@@ -13,4 +13,13 @@ We will create a crack for zip folders and individual files
 
 References:
 
-   - [First Reference](www.google.ca)
+-[Output terminal output into Python variable](https://stackoverflow.com/questions/2449250/any-way-to-assign-terminal-output-to-variable-with-python)
+-[Python execute command in terminal](https://stackoverflow.com/questions/3730964/python-script-execute-commands-in-terminal/3731000)
+-[Remove trailing newline](https://stackoverflow.com/questions/275018/how-can-i-remove-a-trailing-newline-in-python)
+-[Changing command input for saving terminal output](https://cmdlinetips.com/2014/03/how-to-run-a-shell-command-from-python-and-get-the-output/)
+-[How to find files in ubuntu system](http://knoxd3.blogspot.com/2013/06/how-to-find-files-in-kali-linux.html)
+-[Convert terminal output from bytes to string](https://www.science-emergence.com/Articles/How-to-convert-bytes-type-to-string-type-in-python/)
+-[]()
+-[]()
+-[]()
+-[]()
--- python/pdfcrack.py
+++ python/pdfcrack.py
@@ -1,10 +1,25 @@
 #! /usr/bin/python
 import os
+import subprocess
 
-#os.system("pwd")
-userfile = input("Input the full name of the file and extension: ")
+#HAVE PROGRAM DECIDE WHICH CRACK IT IS DOING
+
+
+
+#os.system("pwd") #test that this command works
+userfile = input("Input the full name of the file: ")  #prompts user for inpupt
 type(userfile)
-#print(userfile)
-#crackPath = os.system("locate " + userfile)
-#print(crackPath)
-os.system("pdfcrack -f " + userfile + " -w /usr/share/wordlists/rockyou.txt")
+#print(userfile) #test to see user input was saved into variable
+userfile = userfile + ".pdf" #adds .pdf to the end of the user input
+#print(userfile) #test to make sure .pdf was added
+#os.system("pdfcrack -f " + userfile + " -w /usr/share/wordlists/rockyou.txt") #original command
+#proc=subprocess.Popen('readlink -f ' + userfile, shell=True, stdout=subprocess.PIPE, ) #original saving output
+proc=subprocess.Popen('find ~ -name ' + userfile, shell=True, stdout=subprocess.PIPE, ) #does subprocess, first quotation is the command and subprocess is saved with stdout
+output=proc.communicate()[0] #saves the terminal output
+#print(output) #locate file
+output2 = output.decode("utf-8") #converts the terminal output which is in bytes into a string
+output2 = output2.rstrip() #strips the \n from the end of the string
+#print(output2) #confirm that the path is saved correctly
+#userfilePATH = output2 + " " + userfile
+#print(userfilePATH)
+os.system("pdfcrack -f " + output2 + " -w /usr/share/wordlists/rockyou.txt") #command that cracks the password, calls on pdfcrack
